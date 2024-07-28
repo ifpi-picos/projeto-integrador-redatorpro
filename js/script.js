4c11e5b452f25790d233cbe42126d976105f586f
João M. M. Leal <160866309+JMarcosMoura@users.noreply.github.com>
2024-07-28 18:17:23 -0300

    animações tela home script.js

---
+++ js/script.js
@@ -0,0 +1,29 @@
+ScrollReveal().reveal('.btn', {
+    origin: 'left',
+    duraction: 4000,
+    distance: '40%',
+});
+
+ScrollReveal().reveal('.textInicial', {
+    duraction: 4000,
+    distance: '40%',
+});
+
+ScrollReveal().reveal('.fea-box', {
+    origin: 'left',
+    duraction: 4000,
+    distance: '40%',
+});
+
+ScrollReveal().reveal('.h1lp', {
+    origin: 'left',
+    duraction: 4000,
+    distance: '40%',
+});
+
+ScrollReveal().reveal('.pazul', {
+    origin: 'right',
+    duraction: 4000,
+    distance: '40%',
+});
+

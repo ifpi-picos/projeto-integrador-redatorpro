9c0f2132923f2d2e332145e90cc7fb7d4b5be976
semm00 <sofiasemmemily@gmail.com>
2024-08-05 03:13:44 -0300

    script.js

--- js/script.js
+++ js/script.js
@@ -57,3 +57,37 @@ ScrollReveal().reveal('.btn3', {
     delay: 2000,
 });
 
+class MobileNavbar {
+    constructor(mobileMenu, navList, closeMenu) {
+        this.mobileMenu = document.querySelector(mobileMenu);
+        this.navList = document.querySelector(navList);
+        this.closeMenu = document.querySelector(closeMenu);
+        this.activeClass = 'active';
+    }
+
+    handleClick() {
+        this.navList.classList.toggle(this.activeClass);
+        this.mobileMenu.classList.toggle(this.activeClass);
+        this.closeMenu.classList.toggle(this.activeClass);
+    }
+
+    addClickEvent() {
+        this.mobileMenu.addEventListener("click", this.handleClick.bind(this));
+        this.closeMenu.addEventListener("click", this.handleClick.bind(this));
+    }
+
+    init(){
+        if (this.mobileMenu && this.closeMenu) {
+            this.addClickEvent();
+        }
+
+        return this;
+    }
+}
+
+const mobileNavbar = new MobileNavbar(
+    '#menu-btn',
+    '.navlp .navigation ul',
+    '#menu-close'
+);
+mobileNavbar.init();

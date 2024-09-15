04127468a10932efc5ce61710d32b4e54f8c30e1
JMarcosMoura <j.marcos.mla@gmail.com>
2024-09-15 15:59:21 -0300

    atualizando home...

---
+++ js/scroll-reveal.js
@@ -0,0 +1,29 @@
+ScrollReveal().reveal('.mensagem', {
+    delay: 400,
+    duraction: 4000,
+    distance: '10%',
+});
+
+ScrollReveal().reveal('.mensagem-logo', {
+    delay: 800,
+    duraction: 4000,
+    distance: '10%',
+});
+
+ScrollReveal().reveal('.mensagem-pro', {
+    delay: 1600,
+    duraction: 4000,
+    distance: '10%',
+});
+
+ScrollReveal().reveal('.tutorial', {
+    delay: 1600,
+    duraction: 4000,
+    distance: '40%',
+});
+
+ScrollReveal().reveal('.icon-tutorial', {
+    delay: 1800,
+    duraction: 4000,
+    distance: '40%',
+})

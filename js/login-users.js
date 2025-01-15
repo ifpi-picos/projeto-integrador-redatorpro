f75e5cb6af91b7277222028aa1067e96a0663c72
João M. M. Leal <j.marcos.mla@gmail.com>
2025-01-15 13:16:39 -0300

    Certo

--- js/login-users.js
+++ js/login-users.js
@@ -1,14 +1,3 @@
-// Função para verificar se o usuário já está logado
-function checkLoginStatus() {
-    if (localStorage.getItem('isLoggedIn') === 'true') {
-        // Se estiver logado, redireciona para a página principal
-        window.location.href = 'www/index.html';
-    }
-}
-
-// Verifica se o usuário já está logado
-checkLoginStatus();
-
 // VALIDAR LOGIN
 document.querySelector('.sign-in-form').addEventListener('submit', async function (event) {
     event.preventDefault(); // Impede o envio padrão do formulário
@@ -24,10 +13,6 @@ document.querySelector('.sign-in-form').addEventListener('submit', async functio
         const user = users.find(user => user.email === email && user.password === password);
 
         if (user) {
-            // Define o status de login no localStorage
-            localStorage.setItem('isLoggedIn', 'true');
-
-            // Redireciona para a página principal
             window.location.href = 'www/index.html';
         } else {
             alert('Email ou senha inválidos. Por favor, tente novamente.');

bea60e1d6babfc08a0eaf1330d371460d360c23a
João M. M. Leal <j.marcos.mla@gmail.com>
2025-01-19 11:58:55 -0300

    222

--- js/menu-lateral.js
+++ js/menu-lateral.js
@@ -1,6 +1,24 @@
 document.getElementById('open_btn').addEventListener('click', function () {
-    document.getElementById('sidebar').classList.toggle('open-sidebar');
+    const sidebar = document.getElementById('sidebar');
+    
+    // Alterna a classe 'open-sidebar' para abrir/fechar a sidebar
+    sidebar.classList.toggle('open-sidebar');
+    
+    // Verifica se a sidebar está aberta
+    if (sidebar.classList.contains('open-sidebar')) {
+        const loggedUser = JSON.parse(localStorage.getItem('loggedUser'));
+
+        if (loggedUser) {
+            // Atualiza o nome e o email do usuário na sidebar
+            document.getElementById('user.name').textContent = loggedUser.name;
+            document.getElementById('user.email').textContent = loggedUser.email;
+        } else {
+            // Redireciona para a página de login se não houver usuário logado
+            window.location.href = 'https://ifpi-picos.github.io/projeto-integrador-redatorpro/login';
+        }
+    }
 });
+
 
 // Seleciona todos os elementos que possuem a classe "side-item"
 const sideItems = document.querySelectorAll('.side-item');

63b090b509bda34f396061e6b0e37862853904e9
João M. M. Leal <j.marcos.mla@gmail.com>
2025-02-22 17:58:13 -0300

    Ajustes no js e css de "temas"

--- www/js/routes.js
+++ www/js/routes.js
@@ -276,6 +276,9 @@ var app = new Framework7({
         },
         pageInit: function (event, page) {
           // Fazer algo quando a página de login for inicializada
+          $.getScript('js/temas.js')
+            //.done(() => window.initFolha?.())
+            //.fail(() => console.error("Erro ao carregar folha.js"));
         },
         pageBeforeRemove: function (event, page) {
           // Fazer algo antes de a página ser removida do DOM

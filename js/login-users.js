7a3b74f83b87b8669670ff21be0a3c0e5d1cae9c
semm00 <sofiasemmemily@gmail.com>
2025-02-26 17:40:38 -0300

    teste 2051

--- js/login-users.js
+++ js/login-users.js
@@ -5,29 +5,34 @@ document.querySelector('.sign-in-form').addEventListener('submit', async functio
     const password = document.querySelector('.input-wrap input[type="password"]').value;
 
     try {
+        // Envia as credenciais para a rota de login no backend
         const response = await fetch('https://express-e3hm.onrender.com/users', {
             method: 'POST',
             headers: {
-               'Content-Type': 'application/json'
+                'Content-Type': 'application/json',
             },
-            body: JSON.stringify({ email, password }),
-            credentials: 'include' // Para manter a sessão se for necessário
+            body: JSON.stringify({
+                email,
+                password,
+            }),
+            credentials: 'include', // Para enviar cookies de sessão
         });
 
-        if (!response.ok) {
-            const errorData = await response.json();
-            alert(errorData.error || 'Email ou senha inválidos. Tente novamente.');
-            return;
+        const data = await response.json();
+
+        if (response.ok) {
+            // Se o login for bem-sucedido, armazena as informações no localStorage
+            localStorage.setItem('loggedUser', JSON.stringify({ name: data.user.name, email: data.user.email }));
+
+            // Redireciona para a página principal
+            window.location.href = 'https://ifpi-picos.github.io/projeto-integrador-redatorpro/www/index.html';
+        } else {
+            alert(data.error || 'Email ou senha inválidos. Por favor, tente novamente.');
         }
 
-        const data = await response.json();
-        // Armazena os dados do usuário no localStorage, se necessário
-        localStorage.setItem('loggedUser', JSON.stringify({ name: data.user.name, email: data.user.email }));
-
-        // Redireciona para a página principal
-        window.location.href = 'https://ifpi-picos.github.io/projeto-integrador-redatorpro/www/index.html';
     } catch (error) {
         console.error('Erro ao conectar ao backend:', error);
         alert('Ocorreu um erro ao tentar se conectar ao servidor. Tente novamente mais tarde.');
     }
 });
+

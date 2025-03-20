5149bf1727eba963ec1b637bc86955e7d6acb889
semm00 <sofiasemmemily@gmail.com>
2025-03-20 10:02:38 -0300

    alterações

--- www/js/repertorios.js
+++ www/js/repertorios.js
@@ -4,16 +4,21 @@ document.addEventListener('DOMContentLoaded', () => {
     boxes.forEach(box => {
         box.addEventListener('mouseover', () => {
             box.style.cursor = 'pointer';
-            // Remove a animação sem deixar o elemento invisível
             box.style.animation = 'none';
-            box.style.opacity = '1'; // Força a opacidade para 1
-            // Aplica o efeito de scale mantendo translateY(0)
+            box.style.opacity = '1';
             box.style.transform = 'translateY(0) scale(1.05)';
         });
 
         box.addEventListener('mouseout', () => {
-            // Retorna ao transform original
             box.style.transform = 'translateY(0) scale(1)';
+        });
+
+        // Adiciona evento de clique para redirecionamento
+        box.addEventListener('click', function () {
+            const page = this.getAttribute('data-page');
+            if (page) {
+                window.location.href = page;
+            }
         });
     });
 });

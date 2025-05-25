d76108f8bace9ae5c662328d09c8302439373dfa
João M. M. Leal <j.marcos.mla@gmail.com>
2025-05-24 22:36:42 -0300

    teste

--- www/js/detalhes.js
+++ www/js/detalhes.js
@@ -88,10 +88,9 @@ function compartilharPerfil() {
 
     const appUrl = "https://ifpi-picos.github.io/projeto-integrador-redatorpro/www/index.html";
     const mensagem = `🌟 Olha quem está no RedatorPro! 🌟\n\n` +
-                     `👤 ${item.nome}\n` +
-                     `📚 ${item.especialidade}\n` +
+                     `😌 ${item.nome}\n` +
+                     ` ${item.especialidade}\n` +
                      `⭐ Avaliação: ${item.rating}/5 (${item.rewies} reviews)\n\n` +
-                     `💬 "${truncateDescription(item.descrição, 80)}"\n\n` +
                      `🔍 Conheça este e outros corretores no RedatorPro!\n` +
                      `${appUrl}`;
 

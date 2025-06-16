8868bd82b8754175bad6f5c7bcce86a4398bf30e
semm00 <sofiasemmemily@gmail.com>
2025-06-16 16:34:21 -0300

    perfil ajustes

--- www/js/perfil.js
+++ www/js/perfil.js
@@ -27,14 +27,28 @@ function carregarPerfil() {
             console.log('[carregarPerfil] Dados recebidos:', data);
             if (data && data.name) {
                 $('#profileName').text(data.name);
+                // Atualiza o botão do Instagram para abrir o link do usuário
+                if (data.instagram) {
+                    $('.social-btn')
+                        .attr('href', 'https://instagram.com/' + data.instagram)
+                        .attr('target', '_blank')
+                        .show();
+                } else {
+                    $('.social-btn')
+                        .attr('href', '#')
+                        .removeAttr('target')
+                        .hide();
+                }
             } else {
                 $('#profileName').text('Nome não encontrado');
+                $('.social-btn').hide();
             }
             $('#profileTipo').text(data.tipo ? (data.tipo.charAt(0).toUpperCase() + data.tipo.slice(1)) : '');
             $('#totalRedacoes').text(data.totalRedacoes !== undefined ? data.totalRedacoes : '0');
             $('#ultimaNota').text(data.ultimaNota !== null && data.ultimaNota !== undefined ? data.ultimaNota : '-');
             if (data.fotoPerfil) $('#profileImg').attr('src', data.fotoPerfil);
-            $('#instagramSpan').text(data.instagram ? '@' + data.instagram : 'Adicionar Instagram');
+            // Mostra o nome do usuário do app no lugar do Instagram
+            $('#instagramSpan').text(data.name || '');
             $('#descricaoPerfil').text(data.descricao || 'Clique no lápis para editar sua descrição.');
             $('#descricaoInput').val(data.descricao || '');
         },
@@ -45,6 +59,8 @@ function carregarPerfil() {
             $('#totalRedacoes').text('0');
             $('#ultimaNota').text('-');
             $('#descricaoPerfil').text('Clique no lápis para editar sua descrição.');
+            $('.social-btn').hide();
+            $('#instagramSpan').text('');
             if (xhr.status === 401) {
                 alert('Sua sessão expirou. Faça login novamente.');
                 // Redirecionar para login se desejar
@@ -60,17 +76,14 @@ $('#editProfileBtn').on('click', function(e) {
         // Ativar edição
         editando = true;
         $('#profileName').attr('contenteditable', true).focus();
-        $('#instagramSpan').hide();
-        $('#instagramInput').val($('#instagramSpan').text().replace('@','')).show().focus();
         $('#profileAvatar').css('cursor', 'pointer');
         $('#descricaoPerfil').hide();
         $('#descricaoInput').val($('#descricaoPerfil').text()).show().focus();
+        // Permitir edição do nome
     } else {
         // Salvar edição
         editando = false;
         $('#profileName').attr('contenteditable', false);
-        $('#instagramInput').hide();
-        $('#instagramSpan').show();
         $('#profileAvatar').css('cursor', 'default');
         $('#descricaoPerfil').show();
         $('#descricaoInput').hide();
@@ -96,20 +109,21 @@ $('#fotoPerfilInput').on('change', function(e) {
 function salvarPerfil() {
     const formData = new FormData();
     formData.append('name', $('#profileName').text());
-    formData.append('instagram', $('#instagramInput').val());
+    // Instagram não é mais editável aqui, mas pode ser mantido se quiser permitir edição futura
+    // formData.append('instagram', $('#instagramInput').val());
     formData.append('descricao', $('#descricaoInput').val());
     const file = $('#fotoPerfilInput')[0].files[0];
     if (file) formData.append('fotoPerfil', file);
 
     console.log('[salvarPerfil] Enviando dados:', {
         name: $('#profileName').text(),
-        instagram: $('#instagramInput').val(),
+        // instagram: $('#instagramInput').val(),
         descricao: $('#descricaoInput').val(),
         file: file ? file.name : null
     });
 
     $.ajax({
-        url: 'https://express-e3hm.onrender.com/perfil', // ajuste aqui também!
+        url: 'https://express-e3hm.onrender.com/perfil',
         method: 'PUT',
         headers: { Authorization: 'Bearer ' + getToken() },
         data: formData,
@@ -118,9 +132,21 @@ function salvarPerfil() {
         success: function(data) {
             console.log('[salvarPerfil] Perfil atualizado:', data);
             $('#profileName').text(data.name);
-            $('#instagramSpan').text(data.instagram ? '@' + data.instagram : 'Adicionar Instagram');
+            $('#instagramSpan').text(data.name || '');
             if (data.fotoPerfil) $('#profileImg').attr('src', data.fotoPerfil);
             $('#descricaoPerfil').text(data.descricao || 'Clique no lápis para editar sua descrição.');
+            // Atualiza o botão do Instagram
+            if (data.instagram) {
+                $('.social-btn')
+                    .attr('href', 'https://instagram.com/' + data.instagram)
+                    .attr('target', '_blank')
+                    .show();
+            } else {
+                $('.social-btn')
+                    .attr('href', '#')
+                    .removeAttr('target')
+                    .hide();
+            }
             alert('Perfil atualizado!');
         },
         error: function(xhr) {
@@ -132,7 +158,4 @@ function salvarPerfil() {
 
 $(document).ready(function() {
     carregarPerfil();
-    $('#instagramInput').on('blur', function() {
-        $('#instagramSpan').text('@' + $(this).val());
-    });
 });

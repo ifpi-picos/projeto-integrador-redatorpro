2ce1c7fa38d2bc50b11c54aea97756af9ad9ef85
João M. M. Leal <j.marcos.mla@gmail.com>
2025-06-28 17:31:53 -0300

    Ajustes

--- html/siderbar/siderbar.js
+++ html/siderbar/siderbar.js
@@ -47,3 +47,26 @@ sideItemsNovo.forEach(item => {
         item.classList.add('active');
     });
 });
+
+
+
+// Seleciona o botão de dark mode
+    const darkModeToggle = document.querySelector('.dark-mode-toggle');
+    
+    // Adiciona o evento de clique
+    darkModeToggle.addEventListener('click', function(e) {
+        e.preventDefault(); // Previne o comportamento padrão do link
+        document.body.classList.toggle('dark-theme'); // Alterna a classe dark-theme no body
+        
+        // Opcional: Salvar a preferência no localStorage
+        if (document.body.classList.contains('dark-theme')) {
+            localStorage.setItem('darkMode', 'enabled');
+        } else {
+            localStorage.setItem('darkMode', 'disabled');
+        }
+    });
+    
+    // Opcional: Verificar a preferência salva ao carregar a página
+    if (localStorage.getItem('darkMode') === 'enabled') {
+        document.body.classList.add('dark-theme');
+    }

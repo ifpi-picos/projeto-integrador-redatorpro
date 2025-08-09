6096cf7a6d7440df2bef4cfe87a11b00af9291b6
semm00 <sofiasemmemily@gmail.com>
2025-08-09 03:09:29 -0300

    TESTE

--- www/js/iacorretor.js
+++ www/js/iacorretor.js
@@ -260,12 +260,13 @@ window.initIACorretor = function () {
             const data = await resp.json();
             if (resp.ok) {
                 console.log('[RedatorPro] Redação enviada para o corretor! Redirecionando para pendentes...');
-                // Redireciona para pendentes.html usando Framework7 corretamente
-                if (window.app && app.views && app.views.main && app.views.main.router) {
-                    app.views.main.router.navigate('/pendentes/', { reloadCurrent: true });
-                } else {
-                    window.location.href = 'pendentes.html';
-                }
+                setTimeout(() => {
+                    if (window.app && app.views && app.views.main && app.views.main.router) {
+                        app.views.main.router.navigate('/pendentes/');
+                    } else {
+                        window.location.href = 'pendentes.html';
+                    }
+                }, 200);
             } else {
                 alert(data.error || 'Erro ao enviar para o corretor.');
             }
